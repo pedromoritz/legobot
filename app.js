caa8d55859798a3f52fd365c12a4087455808582
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2020-12-02 18:11:51 -0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 const express = require("express");
 const port = 3000;
 
+var rpio = require('rpio');
+
+MOTOR_RIGHT_FRONT = 12;
+MOTOR_RIGHT_REAR = 8;
+MOTOR_LEFT_FRONT = 16;
+MOTOR_LEFT_REAR = 10;
+
+ON = 0;
+OFF = 1;
+
+const args = process.argv.slice(2)[0];
+
+rpio.open(MOTOR_RIGHT_FRONT, rpio.OUTPUT, OFF);
+rpio.open(MOTOR_RIGHT_REAR, rpio.OUTPUT, OFF);
+rpio.open(MOTOR_LEFT_FRONT, rpio.OUTPUT, OFF);
+rpio.open(MOTOR_LEFT_REAR, rpio.OUTPUT, OFF);
+ 
+function motorRightFront(){
+    motorRightDisable();
+    rpio.open(MOTOR_RIGHT_FRONT, rpio.OUTPUT, ON);
+}
+
+function motorRightRear(){
+    motorRightDisable();
+    rpio.open(MOTOR_RIGHT_REAR, rpio.OUTPUT, ON);
+}
+
+function motorRightDisable() {
+    rpio.open(MOTOR_RIGHT_FRONT, rpio.OUTPUT, OFF);
+    rpio.open(MOTOR_RIGHT_REAR, rpio.OUTPUT, OFF);
+}
+
+function motorLeftFront(){
+    motorLeftDisable();
+    rpio.open(MOTOR_LEFT_FRONT, rpio.OUTPUT, ON);
+}
+
+function motorLeftRear(){
+    motorLeftDisable();
+    rpio.open(MOTOR_LEFT_REAR, rpio.OUTPUT, ON);
+}
+
+function disableMotorLeft() {
+    rpio.open(MOTOR_LEFT_FRONT, rpio.OUTPUT, OFF);
+    rpio.open(MOTOR_LEFT_REAR, rpio.OUTPUT, OFF);
+}
+
+
 (async () => {
     try {
 
@@ -10,6 +58,14 @@ const port = 3000;
         app.set('views', __dirname + '/');
         app.get("/", function(req, res) {
             res.render('index.html');
+        });
+
+        app.get("/motorRightFront", function(req, res) {
+            motorRightFront();
+        });
+
+        app.get("/motorRightDisable", function(req, res) {
+            motorRightDisable();
         });
 
         app.use(express.static(__dirname + '/'));
